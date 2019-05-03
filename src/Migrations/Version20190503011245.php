07c3cd2645dd62469559737fd00c3390e44b3a87
Heissen Lopez <heissen.lopez@gmail.com>
2019-05-02 20:57:03 -0500

    Initial settings to use database (sqllite) and created the initial admin page.

---
+++ src/Migrations/Version20190503011245.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190503011245 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_art FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, slug VARCHAR(100) NOT NULL COLLATE BINARY, content CLOB DEFAULT NULL COLLATE BINARY, published_art DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_art) SELECT id, title, slug, content, published_art FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX UNIQ_23A0E66989D9B62');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_art FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content CLOB DEFAULT NULL, published_art DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_art) SELECT id, title, slug, content, published_art FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+    }
+}
