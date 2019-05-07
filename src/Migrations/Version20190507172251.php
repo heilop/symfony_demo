9dec55560d175f65912d341cd87d3861ca05d3e6
Heissen Lopez <heissen.lopez@gmail.com>
2019-05-07 13:09:43 -0500

    Add timestampable for dummy content.

---
+++ src/Migrations/Version20190507172251.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190507172251 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX UNIQ_23A0E66989D9B62');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, slug VARCHAR(100) NOT NULL COLLATE BINARY, content CLOB DEFAULT NULL COLLATE BINARY, published_art DATETIME DEFAULT NULL, author VARCHAR(255) DEFAULT NULL COLLATE BINARY, heart_count INTEGER DEFAULT NULL, image_filename VARCHAR(255) DEFAULT NULL COLLATE BINARY, created_at DATETIME NOT NULL, update_at DATETIME NOT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at) SELECT id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX UNIQ_23A0E66989D9B62');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content CLOB DEFAULT NULL, published_art DATETIME DEFAULT NULL, author VARCHAR(255) DEFAULT NULL, heart_count INTEGER DEFAULT NULL, image_filename VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT NULL, update_at DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at) SELECT id, title, slug, content, published_art, author, heart_count, image_filename, created_at, update_at FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+}
