5b5255576faa7cc60597906494add82803cb4cb7
Heissen Lopez <heissen.lopez@gmail.com>
2019-05-09 20:41:44 -0500

    Include join to the query and solve N+1.

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -31,10 +31,12 @@ class CommentRepository extends ServiceEntityRepository
    * @return Comment[]
    */
     public function finAllWithSearch(?string $term) {
-      $qb = $this->createQueryBuilder('c');
+      $qb = $this->createQueryBuilder('c')
+        ->innerJoin('c.article', 'a')
+        ->addSelect('a');
 
       if ($term) {
-        $qb->andWhere('c.content LIKE :term OR c.authorName LIKE :term')
+        $qb->andWhere('c.content LIKE :term OR c.authorName LIKE :term OR a.title LIKE :term')
           ->setParameter('term', '%' . $term . '%');
       }
 
