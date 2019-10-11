f94a3241543d9d696062ee5dd6692f0f4aafc7f3
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-11 17:35:12 -0500

    Add password serialize option.

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,13 +4,28 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture {
-  protected function loadData(ObjectManager $manager) {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    protected function loadData(ObjectManager $manager) {
     $this->createManyTmp(10, 'main_users', function ($i) {
       $user = new User();
       $user->setEmail($this->faker->safeEmail);
       $user->setFirstName($this->faker->firstName);
+      $user->setPassword($this->passwordEncoder->encodePassword(
+        $user,
+        'pass'
+      ));
       return $user;
     });
 
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -31,11 +32,16 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator {
    * @var CsrfTokenManagerInterface
    */
   private $csrfTokenManager;
+  /**
+   * @var UserPasswordEncoderInterface
+   */
+  private $passwordEncoder;
 
-  public function __construct(UserRepository $userRepository, RouterInterface $router, CsrfTokenManagerInterface $csrfTokenManager) {
+  public function __construct(UserRepository $userRepository, RouterInterface $router, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder) {
     $this->userRepository = $userRepository;
     $this->router = $router;
     $this->csrfTokenManager = $csrfTokenManager;
+    $this->passwordEncoder = $passwordEncoder;
   }
 
   public function supports(Request $request) {
@@ -69,8 +75,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator {
   }
 
   public function checkCredentials($credentials, UserInterface $user) {
-    // Check this when user using password.
-    return TRUE;
+    return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
   }
 
   public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey) {
