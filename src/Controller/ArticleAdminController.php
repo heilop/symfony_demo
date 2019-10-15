f78e2818afa3f2d406a6b28f99ec8084d7fe95e5
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-14 20:06:37 -0500

    Update dropdown and include dynamic roles.

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Article;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class ArticleAdminController extends AbstractController
 {
 
   /**
-   * @Route("/admin/article/new")
+   * @Route("/admin/article/new", name="admin_article_new")
    */
   public function new(EntityManagerInterface $em) {
     // @TODO Create admin page.
