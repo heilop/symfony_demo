6bc6afa4befef31fbaa31a69a9ce23af90b9d4ab
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-15 16:31:55 -0500

    Include fucntion to get user avatar and update templates.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -143,4 +143,14 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getAvatarUrl(string $size = null): string {
+      $url = 'https://robohash.org/'.$this->getEmail();
+
+      if ($size) {
+        $url .= sprintf('?size=%dx%d', $size, $size);
+      }
+
+      return $url;
+    }
 }
