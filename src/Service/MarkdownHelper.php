250261118062ef7298f1f9799221dc4062a874e9
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-16 22:19:19 -0500

    GetUser logger included on Helper Class.

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -12,22 +12,30 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper {
   private $cache;
   private $markdown;
   private $logger;
+  /**
+   * @var Security
+   */
+  private $security;
 
 
-  public function __construct( AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger) {
+  public function __construct( AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, Security $security) {
     $this->cache = $cache;
     $this->markdown = $markdown;
     $this->logger = $markdownLogger;
+    $this->security = $security;
   }
 
   public function parse(string $source): string {
     if (stripos($source, 'bacon') !== false) {
-      $this->logger->info('They are talking about bacon again!');
+      $this->logger->info('They are talking about bacon again!', [
+        'user' => $this->security->getUser()
+      ]);
     }
 
     $item = $this->cache->getItem('markdown_'.md5($source));
