3c15c9b50cb78f6dd514f0093981ef822c68cce2
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-18 17:36:05 -0500

    Add ApiToken class.

---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ApiTokenRepository")
+ */
+class ApiToken
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $expireAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="apiTokens")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getExpireAt(): ?\DateTimeInterface
+    {
+        return $this->expireAt;
+    }
+
+    public function setExpireAt(?\DateTimeInterface $expireAt): self
+    {
+        $this->expireAt = $expireAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20191018223221.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191018223221 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE api_token (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, token VARCHAR(255) NOT NULL, expire_at DATETIME DEFAULT NULL)');
+        $this->addSql('CREATE INDEX IDX_7BA2F5EBA76ED395 ON api_token (user_id)');
+        $this->addSql('DROP INDEX IDX_919694F9BAD26311');
+        $this->addSql('DROP INDEX IDX_919694F97294869C');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article_tag AS SELECT article_id, tag_id FROM article_tag');
+        $this->addSql('DROP TABLE article_tag');
+        $this->addSql('CREATE TABLE article_tag (article_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, PRIMARY KEY(article_id, tag_id), CONSTRAINT FK_919694F97294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_919694F9BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO article_tag (article_id, tag_id) SELECT article_id, tag_id FROM __temp__article_tag');
+        $this->addSql('DROP TABLE __temp__article_tag');
+        $this->addSql('CREATE INDEX IDX_919694F9BAD26311 ON article_tag (tag_id)');
+        $this->addSql('CREATE INDEX IDX_919694F97294869C ON article_tag (article_id)');
+        $this->addSql('DROP INDEX IDX_9474526C7294869C');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__comment AS SELECT id, article_id, author_name, content, created_at, updated_at, is_deleted FROM comment');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER NOT NULL, author_name VARCHAR(255) NOT NULL COLLATE BINARY, content CLOB NOT NULL COLLATE BINARY, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, is_deleted BOOLEAN NOT NULL, CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO comment (id, article_id, author_name, content, created_at, updated_at, is_deleted) SELECT id, article_id, author_name, content, created_at, updated_at, is_deleted FROM __temp__comment');
+        $this->addSql('DROP TABLE __temp__comment');
+        $this->addSql('CREATE INDEX IDX_9474526C7294869C ON comment (article_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE api_token');
+        $this->addSql('DROP INDEX IDX_919694F97294869C');
+        $this->addSql('DROP INDEX IDX_919694F9BAD26311');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article_tag AS SELECT article_id, tag_id FROM article_tag');
+        $this->addSql('DROP TABLE article_tag');
+        $this->addSql('CREATE TABLE article_tag (article_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, PRIMARY KEY(article_id, tag_id))');
+        $this->addSql('INSERT INTO article_tag (article_id, tag_id) SELECT article_id, tag_id FROM __temp__article_tag');
+        $this->addSql('DROP TABLE __temp__article_tag');
+        $this->addSql('CREATE INDEX IDX_919694F97294869C ON article_tag (article_id)');
+        $this->addSql('CREATE INDEX IDX_919694F9BAD26311 ON article_tag (tag_id)');
+        $this->addSql('DROP INDEX IDX_9474526C7294869C');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__comment AS SELECT id, article_id, author_name, content, is_deleted, created_at, updated_at FROM comment');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER NOT NULL, author_name VARCHAR(255) NOT NULL, content CLOB NOT NULL, is_deleted BOOLEAN NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL)');
+        $this->addSql('INSERT INTO comment (id, article_id, author_name, content, is_deleted, created_at, updated_at) SELECT id, article_id, author_name, content, is_deleted, created_at, updated_at FROM __temp__comment');
+        $this->addSql('DROP TABLE __temp__comment');
+        $this->addSql('CREATE INDEX IDX_9474526C7294869C ON comment (article_id)');
+    }
+}
