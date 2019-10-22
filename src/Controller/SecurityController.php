ea46ef37f16ef38aa125b7337bd465848851736f
Heissen Lopez <heissen.lopez@gmail.com>
2019-10-21 20:12:18 -0500

    Manual registration and login authentication.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\GuardAuthenticationFactory;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -29,5 +35,34 @@ class SecurityController extends AbstractController
     public function logout() {
       throw new \Exception('Will be intercepted before getting here');
 
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator) {
+
+      if ($request->isMethod('POST')) {
+        $user = new User();
+        $user->setEmail($request->request->get('email'));
+        $user->setFirstName('Mistery');
+
+        $user->setPassword($passwordEncoder->encodePassword(
+          $user,
+          $request->request->get("password")
+        ));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $guardHandler->authenticateUserAndHandleSuccess(
+          $user,
+          $request,
+          $formAuthenticator,
+          'main'
+        );
+      }
+      return $this->render('security/register.html.twig');
     }
 }
